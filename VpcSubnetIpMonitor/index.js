2513bf0c1050285a25b1a4da2f6c4b404c8a3df1
EC2 Default User <ec2-user@ip-172-31-79-91.ec2.internal>
2017-12-01 16:37:57 +0000

    Lambda function

---
+++ VpcSubnetIpMonitor/index.js
@@ -0,0 +1,82 @@
+'use strict';
+
+console.log('Loading function');
+
+var AWS = require('aws-sdk');
+AWS.config.update({region: 'us-east-1'});
+var ec2 = new AWS.EC2();
+var cw = new AWS.CloudWatch();
+
+var chunk = require('chunk');
+
+/**
+ * Checks all subnets in an account for IP address availability, and reports
+ * metrics on that availability.
+ */
+exports.handler = (event, context, callback) => {
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+    var MetricData = [];
+
+    ec2.describeSubnets({}, function(err,data) {
+        if (err) console.log(err, err.stack);
+        else {
+            console.log("Success { Count:", data['Subnets'].length, "}");
+            
+            data['Subnets'].forEach(function(subnet) {
+                //Available IP Addresses (Count)
+                MetricData.push({
+                    MetricName: "AvailableIpAddressCount",
+                    Dimensions: [
+                        {
+                            Name: "SubnetId",
+                            Value: subnet.SubnetId
+                        }
+                    ],
+                    Unit: 'Count',
+                    Value: subnet.AvailableIpAddressCount
+                });
+                
+                var TotalIpAddressCount = Math.pow(2, 32 - parseInt(subnet.CidrBlock.split('/')[1], 10));
+                
+                //Available IP Addresses (Percent)
+                MetricData.push({
+                    MetricName: "AvailableIpAddressPercent",
+                    Dimensions: [
+                        {
+                            Name: "SubnetId",
+                            Value: subnet.SubnetId
+                        }
+                    ],
+                    Unit: 'Percent',
+                    Value: parseFloat(subnet.AvailableIpAddressCount) / parseFloat(TotalIpAddressCount)
+                });
+                
+                // Total IP Addresses (Count)
+                MetricData.push({
+                    MetricName: "TotalIpAddressCount",
+                    Dimensions: [
+                        {
+                            Name: "SubnetId",
+                            Value: subnet.SubnetId
+                        }
+                    ],
+                    Unit: 'Count',
+                    Value: TotalIpAddressCount
+               });
+               
+            });
+            
+            // putMetricData can only accept 20 metrics at a time.
+            chunk(MetricData, 20).forEach(function (MetricChunk) {
+                cw.putMetricData({Namespace: 'VPC', MetricData: MetricChunk}, function(err, data) {
+                    if (err) console.log(err,err.stack);
+                    else {
+                        console.log("Success", data);
+                    }
+                });
+            });
+        }
+    });
+    
+    callback();
+};
